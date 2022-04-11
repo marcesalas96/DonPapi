f5455ebc60c88a809be35de08bd1698ff749e8f9
marcesalas96 <marcesalas1996@gmail.com>
2022-04-11 02:24:40 -0300

    Agregado de funcionalidad a los botones

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,7 @@
 // Declaracion variables
-let carta = ""
-let opciones = ""
-let plato = []
 let cuentaParcial = 0
 let cuentaTotal = 0
-let comida =""
-let pedido=[]
-let variedades=["Tacos","Burritos","Postres","Bebidas"]
-// Declarando Functions
-function iniciar(){
-    personalizarNombre()
-    crearLista()
-}
-
+let contador = 0
         // Function Constructor
 class Variedad{
     constructor(id, nombre){
@@ -21,35 +10,39 @@ class Variedad{
     }
 }
 class Burritos{
-    constructor (id, tipo, variedad, precio){ 
+    constructor (id, tipo, variedad, precio, categoria){ 
         this.id=id
         this.tipo=tipo
         this.variedad=variedad
         this.precio=precio
+        this.categoria=categoria
     }
 }
 class Tacos{
-    constructor (id, tipo, variedad, precio){ 
+    constructor (id, tipo, variedad, precio, categoria){ 
         this.id=id
         this.tipo=tipo
         this.variedad=variedad
         this.precio=precio
+        this.categoria=categoria
     }
 }
 class Postres{
-    constructor (id, tipo, variedad, precio){ 
+    constructor (id, tipo, variedad, precio, categoria){ 
         this.id=id
         this.tipo=tipo
         this.variedad=variedad
         this.precio=precio
+        this.categoria=categoria
     }
 }
 class Bebidas{
-    constructor (id, tipo, variedad, precio){ 
+    constructor (id, tipo, variedad, precio, categoria){ 
         this.id=id
         this.tipo=tipo
         this.variedad=variedad
         this.precio=precio
+        this.categoria=categoria
     }
 }
             //Construyendo Objetos 
@@ -57,177 +50,50 @@ const variedadBurrito = new Variedad(1,"Burritos")
 const variedadTaco = new Variedad(2,"Tacos")
 const variedadPostre = new Variedad(3,"Postres")
 const variedadBebida = new Variedad(4,"Bebidas")
-const burritoCarne = new Burritos(1,"Burrito","Carne",650)
-const burritoCerdo = new Burritos(2,"Burrito","Cerdo",570)
-const burritoPollo = new Burritos(3,"Burrito","Pollo",500)
-const burritoVeggie = new Burritos(4,"Burrito","Veggie",520)
-const tacoPastor = new Tacos(5,"Taco","Pastor",580)
-const tacoCerdo = new Tacos(6,"Taco","Cerdo",550)
-const tacoMixto = new Tacos(7,"Taco","Pollo y Carne",500)
-const tacoVeggie = new Tacos(8,"Taco","Veggie",400)
-const postreBanana = new Postres(9,"Postre", "Banana con tequila",350)
-const postreBrownie = new Postres(10,"Postre","Brownie con helado",430)
-const postreHelado = new Postres(11,"Postre","Helado de Dulce de leche",350)
-const postreVolcan = new Postres(12,"Postre","Volcan de chocolate",450)
-const bebidaCoca = new Bebidas(13,"Bebida","Coca-Cola",150)
-const bebidaTrago = new Bebidas(14,"Bebida","Margarita",300)
+const burritoCarne = new Burritos(1,"Burrito","Carne",650,1)
+const burritoCerdo = new Burritos(2,"Burrito","Cerdo",570,1)
+const burritoPollo = new Burritos(3,"Burrito","Pollo",500,1)
+const burritoVeggie = new Burritos(4,"Burrito","Veggie",520,1)
+const tacoPastor = new Tacos(5,"Taco","Pastor",580,2)
+const tacoCerdo = new Tacos(6,"Taco","Cerdo",550,2)
+const tacoMixto = new Tacos(7,"Taco","Pollo y Carne",500,2)
+const tacoVeggie = new Tacos(8,"Taco","Veggie",400,2)
+const postreBanana = new Postres(9,"Postre", "Banana con tequila",350,3)
+const postreBrownie = new Postres(10,"Postre","Brownie con helado",430,3)
+const postreHelado = new Postres(11,"Postre","Helado de Dulce de leche",350,3)
+const postreVolcan = new Postres(12,"Postre","Volcan de chocolate",450,3)
+const bebidaCoca = new Bebidas(13,"Bebida","Coca-Cola",150,4)
+const bebidaTrago = new Bebidas(14,"Bebida","Margarita",300,4)
 
 
             //Armando arrays con los objetos 
 
 const menu = []
-menu.push(variedadTaco,variedadBurrito,variedadBebida,variedadPostre)
-let listaBurritos=[]
-listaBurritos.push(burritoCarne,burritoCerdo,burritoPollo,burritoVeggie)
-let listaTacos=[]
-listaTacos.push(tacoPastor,tacoCerdo,tacoMixto,tacoVeggie)
-let listaPostres=[]
-listaPostres.push(postreBanana,postreBrownie,postreHelado,postreVolcan)
-let listaBebidas=[]
-listaBebidas.push(bebidaCoca,bebidaTrago)
+menu.push(variedadBurrito,variedadTaco,variedadPostre,variedadBebida)
+const listaComidas = []
+listaComidas.push(burritoCarne,burritoCerdo,burritoPollo,burritoVeggie,tacoPastor,tacoCerdo,tacoMixto,tacoVeggie,postreBanana,postreBrownie,postreHelado,postreVolcan,bebidaCoca,bebidaTrago)
 
     //Fin Objetos/ Arrays 
-
-function cartaResto(){
-    carta = prompt("Desea ver la carta?")
-    while (carta.toLowerCase() === "si"){
-        mostrarCarta()
-
-        // Creando ciclos para poder dar funcionamiento al programa
-
-        while (opciones.toLowerCase() === "tacos" || opciones.toLowerCase() === "burritos" || opciones.toLowerCase() === "postres" || opciones.toLowerCase() === "bebidas"){
-            if (opciones.toLowerCase() === "tacos"){
-                alert("Seleccione alguna de las siguientes variedades por el numero")
-                for(const elem of listaTacos)
-                alert(elem.id + ") " + elem.variedad + " $" + elem.precio)
-                let seleccion = prompt("Ingrese el numero de su variedad: ")
-                if (seleccion>4 && seleccion<=8){
-                    plato = listaTacos.find((plato)=>plato.id==seleccion)
-                    console.log(plato)
-                    pedido.push(plato)
-                    console.log(pedido)
-                    cuentaParcial+=plato.precio
-                    console.log(cuentaParcial)
-                    pedirComida()
-                }
-                else{
-                    alert("Opcion incorrecta, intente de nuevo")
-                    mostrarCarta()
-                }
-            }
-            else if (opciones.toLowerCase() === "burritos"){ 
-                alert("Seleccione alguna de las siguientes variedades por el numero")
-                for(const elem of listaBurritos)
-                alert(elem.id + ") " + elem.variedad + " $" + elem.precio)
-                let seleccion = prompt("Ingrese el numero de su variedad: ")
-                if (seleccion<=4){
-                    plato=listaBurritos.find((plato)=>plato.id==seleccion)
-                    console.log(plato)
-                    pedido.push(plato)
-                    console.log(pedido)
-                    cuentaParcial+=plato.precio
-                    console.log(cuentaParcial)
-                    pedirComida()
-                }
-                else{
-                    alert("Opcion incorrecta, intente de nuevo")
-                    mostrarCarta()
-                }
-                }
-            else if (opciones.toLowerCase() === "postres"){ 
-                alert("Seleccione alguna de las siguientes variedades por el numero")
-                for(const elem of listaPostres)
-                alert(elem.id + ") " + elem.variedad + " $" + elem.precio)
-                let seleccion = prompt("Ingrese el numero de su variedad: ")
-                if (seleccion<=12 && seleccion>8){
-                    plato=listaPostres.find((plato)=>plato.id==seleccion)
-                    console.log(plato)
-                    pedido.push(plato)
-                    console.log(pedido)
-                    cuentaParcial+=plato.precio
-                    console.log(cuentaParcial)
-                    pedirComida()
-                }
-                else{
-                    alert("Opcion incorrecta, intente de nuevo")
-                    mostrarCarta()
-                }
-                }
-            else if (opciones.toLowerCase() === "bebidas"){ 
-                alert("Seleccione alguna de las siguientes variedades por el numero")
-                for(const elem of listaBebidas)
-                alert(elem.id + ") " + elem.variedad + " $" + elem.precio)
-                let seleccion = prompt("Ingrese el numero de su variedad: ")
-                if (seleccion<=15 && seleccion>12){
-                    plato=listaBebidas.find((plato)=>plato.id==seleccion)
-                    console.log(plato)
-                    pedido.push(plato)
-                    console.log(pedido)
-                    cuentaParcial+=plato.precio
-                    console.log(cuentaParcial)
-                    pedirComida()
-                }
-                else{
-                    alert("Opcion incorrecta, intente de nuevo")
-                    mostrarCarta()
-                }
-            }
-            else{
-                alert("Opcion incorrecta! Intente de nuevo")
-                mostrarCarta()
-            }
-        }
-        if (((opciones.toLowerCase() !== "tacos" || opciones.toLowerCase() !== "burritos" || opciones.toLowerCase() !== "postres" || opciones.toLowerCase() !== "bebidas") && opciones.toLowerCase()=="no")){
-            alert("Gracias por elegirnos!")
-            alert("No olvides abrir tu consola para ver tu pedido!")
-            break
-        }
-        else if (opciones.toLowerCase() !== "tacos" || opciones.toLowerCase() !== "burritos" || opciones.toLowerCase() !== "postres" || opciones.toLowerCase() !== "bebidas"){
-            console.log(opciones)
-            alert("OPCION INCORRECTA, INTENTE DE NUEVO")
-            mostrarCarta()
-        }
-    } 
-    if (carta.toLowerCase() === "no"){
-        alert("Gracias por visitarnos!")
-    }   
-    }
-
-function mostrarCarta(){
-    opciones = prompt(`MENÚ\n
-        TACOS\n
-        BURRITOS\n
-        POSTRES\n
-        BEBIDAS\n `)
-}
-function pedirComida(){
-    comida = prompt ("Desea seguir pidiendo comida?")
-                    if (comida.toLowerCase() === "si"){
-                        mostrarCarta()}
-                    else {
-                        mostrarPedidoFinal()
-                            console.log("El total de su pedido es de: $" + cuentaTotal)
-                            for(elemento of pedido){
-                                console.log("Su pedido es: " + elemento.tipo + " " + elemento.variedad)
-                            }
-                        opciones="no"
-                        
-                    }
-}
+    //Creando Functions
+function iniciar(){
+        personalizarNombre()
+        crearBotones()
+        cerrarPlatos()
+}    
 function personalizarNombre(){
 const nombre=prompt("Ingresa tu nombre!")
 let titulo=document.getElementById("titulo")
-console.log(titulo.innerText)
 titulo.innerText = `Bienvenid@, ${nombre}!`
-console.log(titulo.innerText)
+
 }
-function crearLista(){
-    const listaVariedades = document.querySelector("#listaVariedades")
+function crearBotones(){
+    const btnMenu = document.querySelector("#menu")
     menu.forEach((categoria)=>{
         const miBoton = document.createElement("button");
         miBoton.setAttribute("class", "botonMenu");
         miBoton.innerHTML=categoria.nombre;
-        listaVariedades.appendChild(miBoton)
+        miBoton.addEventListener("click", ()=> mostrarPlatos(categoria.id))
+        btnMenu.appendChild(miBoton)
     })
 }
 function mostrarPedidoFinal(){
@@ -243,7 +109,109 @@ function mostrarPedidoFinal(){
 
 }
 
+function cerrarPlatos(){
+    let div = document.querySelector("#listaMenu");
+    console.log(div)
+    let cerrar= document.querySelectorAll("#div__hijo");
+    cerrar.forEach((nodo)=>{
+        div.removeChild(nodo)})
+    contador=0
+}
+
+
+function mostrarPlatos(idCategoria){
+    const productosFiltrados = listaComidas.filter(comida=> comida.categoria === idCategoria)
+    const div = document.querySelector("#listaMenu")
+    const contenedor =document.createElement("div")
+    if (idCategoria==1){
+        contenedor.setAttribute("class","div__burritos")
+        contenedor.setAttribute("id","div__hijo")
+        div.appendChild(contenedor)
+    }
+    else if (idCategoria==2){
+        contenedor.setAttribute("class","div__tacos")
+        contenedor.setAttribute("id","div__hijo")
+        div.appendChild(contenedor)
+    }
+    else if (idCategoria==3){
+        contenedor.setAttribute("class","div__postres")
+        contenedor.setAttribute("id","div__hijo")
+        div.appendChild(contenedor)
+    }
+    else if (idCategoria==4){
+        contenedor.setAttribute("class","div__bebidas")
+        contenedor.setAttribute("id","div__hijo")
+        div.appendChild(contenedor)
+    }
+    crearMenu(productosFiltrados,idCategoria)
+
+}
+function crearMenu(productosFiltrados,idCategoria){
+    while (idCategoria==1){
+        let cadena = ``
+        productosFiltrados.forEach((plato)=>{
+            cadena +=
+            `<p> 
+            ${plato.id}) ${plato.tipo} ${plato.variedad} precio: $${plato.precio}
+            </p>`
+            document.querySelector(".div__burritos").innerHTML= cadena
+        })
+        contador++
+        if (contador>1){
+            cerrarPlatos()
+        }
+        break
+        
+    }
+    while (idCategoria==2){
+        let cadena = ``
+        productosFiltrados.forEach((plato)=>{
+            cadena +=
+            `<p> 
+            ${plato.id}) ${plato.tipo} ${plato.variedad}, precio: $${plato.precio}
+            </p>`
+            document.querySelector(".div__tacos").innerHTML= cadena
+        })
+        contador++
+        console.log(contador)
+        if (contador>1){
+            cerrarPlatos()
+        }
+        break
+    }
+    while (idCategoria==3){
+        let cadena = ``
+        productosFiltrados.forEach((plato)=>{
+            cadena +=
+            `<p> 
+            ${plato.id}) ${plato.tipo} ${plato.variedad}, precio: $${plato.precio}
+            </p>`
+            document.querySelector(".div__postres").innerHTML= cadena
+        })
+        contador++
+        console.log(contador)
+        if (contador>1){
+            cerrarPlatos()
+        }
+        break
+    }
+    while (idCategoria==4){
+        let cadena = ``
+        productosFiltrados.forEach((plato)=>{
+            cadena +=
+            `<p> 
+            ${plato.id}) ${plato.tipo} ${plato.variedad}, precio: $${plato.precio}
+            </p>`
+            document.querySelector(".div__bebidas").innerHTML= cadena
+        })
+        contador++
+        if (contador>1){
+            cerrarPlatos()
+        }
+        break
+    }
+}
+
 // Fin de declaracion de functions
 // Inicio del codigo ejecutable
 iniciar()
-console.log(menu)
